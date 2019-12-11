cbbca9a44813f7caae274d9f1c6df2df0bac02c8
hy <huyu_scls@163.com>
2019-12-11 11:29:09 +0800

    update: 菜单配置

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -4,17 +4,39 @@
       <img :src="logo" alt="Antd"/>
       <span v-if="!collapse">Preview</span>
     </div>
-    <el-menu mode="inline"
-             default-active="1-1"
+    <el-menu default-active="/exception/404"
+             router
              :collapse="collapse"
-             @open="handleOpen"
-             @close="handleClose">
-      <el-submenu index="1">
+             style="border-right: none"
+             background-color="#001529"
+             text-color="#ffffffa6"
+             active-text-color="#ffffff"
+             unique-opened>
+      <el-submenu index="/form">
         <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
+          <i class="el-icon-edit"></i>
+          <span>表单页</span>
         </template>
-        <el-menu-item index="1-1">菜单1</el-menu-item>
+        <el-menu-item index="/form/basic-form">基础表单</el-menu-item>
+      </el-submenu>
+
+      <el-submenu index="/result">
+        <template slot="title">
+          <i class="el-icon-circle-check"></i>
+          <span>结果页</span>
+        </template>
+        <el-menu-item index="/result/success">成功页</el-menu-item>
+        <el-menu-item index="/result/failure">失败页</el-menu-item>
+      </el-submenu>
+
+      <el-submenu index="/exception">
+        <template slot="title">
+          <i class="el-icon-circle-close"></i>
+          <span>异常页</span>
+        </template>
+        <el-menu-item index="/exception/403">403</el-menu-item>
+        <el-menu-item index="/exception/404">404</el-menu-item>
+        <el-menu-item index="/exception/500">500</el-menu-item>
       </el-submenu>
     </el-menu>
   </div>
@@ -25,19 +47,24 @@ import logo from '../../assets/logo.png'
 
 export default {
   name: 'menu',
+  props: {
+    isMobile: {
+      propTypes: Boolean,
+      required: false,
+      default: false
+    },
+    collapse: {
+      propTypes: Boolean,
+      required: false,
+      default: false
+    }
+  },
   data () {
     return {
-      collapse: false,
       logo
     }
   },
   methods: {
-    handleOpen (key, keyPath) {
-
-    },
-    handleClose (key, keyPath) {
-
-    }
   }
 }
 </script>
